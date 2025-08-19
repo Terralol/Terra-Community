82de66601f1215ac201e0ff57bbd882337ab54d1
Terra Hub <dexhax76@gmail.com>
2025-08-19 14:15:54 +0300

    Update TerraLib.lua

--- Library/TerraLib.lua
+++ Library/TerraLib.lua
@@ -282,11 +282,11 @@ end)
     title.BorderSizePixel = 0
     title.Position = UDim2.new(0.0171428565, 0, 0.344827592, 0)
     title.Size = UDim2.new(0, 204, 0, 8)
-    title.Font = Enum.Font.Gotham
+    title.Font = Enum.Font.FredokaOne
     title.RichText = true
     title.Text = kavName
     title.TextColor3 = Color3.fromRGB(245, 245, 245)
-    title.TextSize = 16.000
+    title.TextSize = 15.000
     title.TextXAlignment = Enum.TextXAlignment.Left
 
     close.Name = "close"
